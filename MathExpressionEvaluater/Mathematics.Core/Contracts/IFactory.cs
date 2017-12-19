626a6629cba659e088cb90bc0e7e97b7de000991
Stilyan Mladenov <spmladenov@sofia-uni.bg>
2017-12-19 16:11:51 +0200

    Factory created but not fully implemented

---
+++ MathExpressionEvaluater/Mathematics.Core/Contracts/IFactory.cs
@@ -0,0 +1,19 @@
+﻿using Mathematics.Operations.Contracts;
+
+namespace Mathematics.Core.Contracts
+{
+    interface IFactory
+    {
+        IOperation<int> CreateIntAdditionOperation();
+        IOperation<decimal> CreateDecimalAdditionOperation();
+
+        IOperation<int> CreateIntSubstractionOperation();
+        IOperation<decimal> CreateDecimalSubstractionOperation();
+
+        IOperation<int> CreateIntDivisionOperation();
+        IOperation<decimal> CreateDecimalDivisionOperation();
+
+        IOperation<int> CreateIntMultiplicationOperation();
+        IOperation<decimal> CreateDecimalMultiplicationOperation();
+    }
+}
---
+++ MathExpressionEvaluater/Mathematics.Core/Engine/Factory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Mathematics.Core.Contracts;
+using Mathematics.Operations.Contracts;
+using Mathematics.Operations.Models;
+
+namespace Mathematics.Core.Engine
+{
+    class Factory : IFactory
+    {
+        public IOperation<int> CreateIntAdditionOperation()
+        {
+            return new IntAddition();
+        }
+
+        public IOperation<decimal> CreateDecimalAdditionOperation()
+        {
+            return new DecimalAddition();
+        }
+
+        public IOperation<int> CreateIntSubstractionOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperation<decimal> CreateDecimalSubstractionOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperation<int> CreateIntDivisionOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperation<decimal> CreateDecimalDivisionOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperation<int> CreateIntMultiplicationOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperation<decimal> CreateDecimalMultiplicationOperation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
