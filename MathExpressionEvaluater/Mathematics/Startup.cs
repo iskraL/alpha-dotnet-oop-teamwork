719ad65cfe8e26de6de3499a58d339e25c753cd8
Iskra Lumbeva <iskra.lumbeva@gmail.com>
2018-01-02 09:56:43 +0200

    Input in main

--- MathExpressionEvaluater/Mathematics/Startup.cs
+++ MathExpressionEvaluater/Mathematics/Startup.cs
@@ -6,17 +6,9 @@ namespace Mathematics
     {
         static void Main()
         {
+            string expression = Console.ReadLine();
             Expression e = new Expression();
-
-            Console.WriteLine(e.Evaluate("("));
-
-            Console.WriteLine("5 + (-4) = " + e.Evaluate("5 + (-4)"));
-            Console.WriteLine("5 + -4 = " + e.Evaluate("5 + -4"));
-
-            Console.WriteLine("-4 * 7 = " + e.Evaluate("-4 * 7"));
-            Console.WriteLine("5 + -4 * 7 = " + e.Evaluate("5 + -4 * 7"));
-
-
+            Console.WriteLine(e.Evaluate(expression));
         }
     }
 }
